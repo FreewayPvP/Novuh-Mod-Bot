5ae579f1bacb707331d4e72ef5bee9cb9198115c
FreewayPvP <34444618+FreewayPvP@users.noreply.github.com>
2018-01-27 13:44:17 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,6 @@ const chalk = require('chalk');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 module.exports = client => {
-  client.user.setGame(`/help | Protecting ${client.guilds.size} servers!`)
+  client.user.setGame(`/help | with Savage`)
   console.log(chalk.bgGreen.black(`Online and ready to serve ${client.guilds.size} servers.`));
 };
